3c0412202921de5086d7c6d3c208fe8a6f3ab222
Maxwelltoo <1391548050@qq.com>
2020-09-12 00:15:52 +0800

    波形控制+

--- Signal_Generator.srcs/sources_1/new/Signal_Generator.v
+++ Signal_Generator.srcs/sources_1/new/Signal_Generator.v
@@ -33,14 +33,25 @@ module Signal_Generator(
     output DAC_SCLK,
     output DAC_DIN);
 
-wire       wav, amp, f_inc, f_dec, p_inc, p_dec;
-//波形控制 波形 幅值 频率+ 频率- 相位+ 相位-
+wire  wav_c, amp_c, f_inc, f_dec, p_inc, p_dec;
+// 改变波形，改变幅值，频率+，频率-，相位+，相位-
 
-Key key_wav(CLK_50M, RST_N, KEY_1, wav); // 按键1消抖后控制波形
-Key key_amp(CLK_50M, RST_N, KEY_2, amp); // 按键2消抖后控制幅值
+Key key_wav(CLK_50M, RST_N, KEY_1, wav_c); // 按键1消抖后控制波形
+Key key_amp(CLK_50M, RST_N, KEY_2, amp_c); // 按键2消抖后控制幅值
 Encoder enc_freq(CLK_50M, RST_N, ENC_1_A, ENC_1_B, f_inc, f_dec); // 编码器1控制频率
 Encoder enc_phas(CLK_50M, RST_N, ENC_2_A, ENC_2_B, p_inc, p_dec); // 编码器2控制相位
 
+// DDS_control 
+reg [1 : 0] wave;
+always @(posedge wav_c or negedge RST_N) begin
+    if (!RST_N)     wave <= 16'h0083;
+    else if (wav_c) wave <= wave + 1;
+end
+reg [1 : 0]  amplitude;
+always @(posedge wav_c or negedge RST_N) begin
+    if (!RST_N)     amplitude <= 16'h0083;
+    else if (wav_c) amplitude <= amplitude + 1;
+end
 reg [15: 0] phase;
 always @(posedge p_inc or posedge p_dec or negedge RST_N) begin
     if (!RST_N)     phase <= 16'h0083;
@@ -57,6 +68,5 @@ end
 reg [11:0] dds_data;  // DDS输出数据
 reg        dds_valid; // DDS输出有效标志
 DAC7512 dac_driver(CLK_50M, RST_N, dds_data, dds_valid, DAC_SYNC, DAC_SCLK, DAC_DIN);
-
 
 endmodule
